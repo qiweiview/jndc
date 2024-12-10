764963143f1569dcb52dd4a6e7a831dedeb6371b
liuqiwei <30173268@qq.com>
2024-12-10 09:26:09 +0800

    - remove unused module

--- jndc-server-h5-latest/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-} satisfies Config;
